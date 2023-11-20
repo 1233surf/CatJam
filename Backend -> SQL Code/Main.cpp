1a1c29c75da42adaf7a6b593db789e6295c22e60
LazzzzyKitty <143209010+LazzzzyKitty@users.noreply.github.com>
2023-11-19 21:35:15 -0500

    Add files via upload

--- Backend -> SQL Code/Main.cpp
+++ Backend -> SQL Code/Main.cpp
@@ -5,13 +5,13 @@
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/prepared_statement.h>
+
 using namespace std;
-//using namespace mysqlx;
-//tcp://localhost.mysql.database.azure.com:3306
-//for demonstration only. never save your password in the code!
-const string server = "CatJam";
-const string username = "root";
-const string password = "456838Rg~";
+
+const string server = "localhost"; // Update the server to your hostname 127.0.0.1
+const string username = "root"; //supriseme
+const string password = "jacobiscool"; // Update the password jake
+const int port = 3306; // Update the port
 
 int main()
 {
@@ -19,7 +19,7 @@ int main()
 	sql::Connection* con;
 	sql::Statement* stmt;
 	sql::PreparedStatement* pstmt;
-	const string url = "mysqlx://root:456838Rg~@localhost:3306";	const string schema = "CatJam";
+
 	try
 	{
 		driver = get_driver_instance();
@@ -32,34 +32,6 @@ int main()
 		exit(1);
 	}
 
-	//please create database "quickstartdb" ahead of time
-	con->setSchema("quickstartdb");
-
-	stmt = con->createStatement();
-	stmt->execute("DROP TABLE IF EXISTS inventory");
-	cout << "Finished dropping table (if existed)" << endl;
-	stmt->execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);");
-	cout << "Finished creating table" << endl;
-	delete stmt;
-
-	pstmt = con->prepareStatement("INSERT INTO inventory(name, quantity) VALUES(?,?)");
-	pstmt->setString(1, "banana");
-	pstmt->setInt(2, 150);
-	pstmt->execute();
-	cout << "One row inserted." << endl;
-
-	pstmt->setString(1, "orange");
-	pstmt->setInt(2, 154);
-	pstmt->execute();
-	cout << "One row inserted." << endl;
-
-	pstmt->setString(1, "apple");
-	pstmt->setInt(2, 100);
-	pstmt->execute();
-	cout << "One row inserted." << endl;
-
-	delete pstmt;
-	delete con;
-	system("pause");
+	
 	return 0;
 }
