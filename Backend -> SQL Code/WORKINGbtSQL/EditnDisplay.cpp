771edae288565d51ee36aeaee97a898c69a77ca7
1233surf <64109360+1233surf@users.noreply.github.com>
2023-12-05 23:08:07 -0500

    ok

--- Backend -> SQL Code/WORKINGbtSQL/EditnDisplay.cpp
+++ Backend -> SQL Code/WORKINGbtSQL/EditnDisplay.cpp
@@ -18,7 +18,7 @@ void Updateinfo(sql::Statement* stmt, sql::ResultSet* res)
 	{
 
 		stmt->execute("USE sys");
-		res = stmt->executeQuery(" UPDATE customerinfo SET CustomerID = '2', CustomerFirstName = 'New', CustomerEmail = 'New@Gmail.com', CustomerUserName = 'HII', CustomerPassword = 'NewP'  Where CustomerID = '1'");
+		res = stmt->executeQuery(" UPDATE customerinfo SET CustomerID = '2', CustomerFirstName = 'New', CustomerEmail = 'New@Gmail.com', CustomerUserName = 'HII', CustomerPassword = 'NewP'  Where CustomerID = '2'");
 		//res = stmt->executeQuery("INSERT INTO customerinfo (CustomerID, CustomerFirstName, CustomerLastName, CustomerEmail, CustomerUserName, CustomerPassword) VALUES ('1', 'Rachel' , 'Parker', 'IloveThisClass@JK.com', 'FakeName', 'FakePassword')");
 
 	}
@@ -35,7 +35,7 @@ void updateinfo1(sql::Statement* stmt, sql::ResultSet* res)
 	{
 
 		stmt->execute("USE sys");
-		res = stmt->executeQuery(" UPDATE employeeinfo SET EmployeeNumber = '2', EmployeeFirstName = 'New', EmployeeLastName = 'NewTo', EmployeeEmail = 'NewEmail@Gmail.com', EmployeeUserName = 'Something', EmployeePassword = 'Shhh'");
+		res = stmt->executeQuery(" UPDATE employeeinfo SET EmployeeNumber = '2', EmployeeFirstName = 'New', EmployeeLastName = 'NewTo', EmployeeEmail = 'NewEmail@Gmail.com', EmployeeUserName = 'Something', EmployeePassword = 'Shhh' Where EmployeeNumber = '1'");
 		//res = stmt->executeQuery("INSERT INTO employeeinfo (EmployeeNumber, EmployeeFirstName, EmployeeLastName, EmployeeEmail, EmployeeUserName, EmployeePassword) VALUES ('1', 'Aaron' , 'Loveland', 'Imtired@reallytired', 'NameisFake', 'PasswordIsTo')");
 	}
 	catch (sql::SQLException e)
@@ -51,7 +51,7 @@ void updateinfo2(sql::Statement* stmt, sql::ResultSet* res)
 	{
 
 		stmt->execute("USE sys");
-		res = stmt->executeQuery(" UPDATE rools SET RoolsID = '2', RoolsName = 'Creditor', RoolsEmail = 'Newrools@gmail.com', RoolsQeury = 'yes'");
+		res = stmt->executeQuery(" UPDATE rools SET RoolsID = '2', RoolsName = 'Creditor', RoolsEmail = 'Newrools@gmail.com', RoolsQeury = 'yes' Where RoolsID = '1'");
 		//res = stmt->executeQuery("INSERT INTO rools (RoolsID, RoolsName, RoolsEmail, RoolsQeury) VALUES ('1', 'Customer' , 'Rools@email.com', 'no')");
 	}
 	catch (sql::SQLException e)
@@ -67,7 +67,7 @@ void updateinfo3(sql::Statement* stmt, sql::ResultSet* res)
 	{
 
 		stmt->execute("USE sys");
-		res = stmt->executeQuery(" UPDATE songinfo SET SongInfoID = '3'");
+		res = stmt->executeQuery(" UPDATE songinfo SET SongInfoID = '3' Where SongInfoID = '1'");
 		//res = stmt->executeQuery("INSERT INTO songinfo (SongInfoID, SongInfoName, SongInfoLyric, SongAlbum, SongArtist, SongProducer) VALUES ('1', 'Binding Lights' , 'Lyrics', 'The Weekend', 'The Weekend', 'IDK')");
 
 	}
